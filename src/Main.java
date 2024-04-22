f67ee0bb3f2f0753b3b0aae14dba274339f07655
Aida <aidagm026@gmail.com>
2024-04-22 21:14:08 +0200

    Cambios en menús

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
+            System.out.print("Selecciona una de estas opciones: ");
             System.out.println("1. Lista de conciertos");
-            System.out.println("2. Buscar información sobre un concierto");
-            System.out.println("3. Salir");
-            System.out.print("Elija una opción: ");
+            System.out.println("2. Buscar información del concierto");
+            System.out.println("3. Cerrar");
+
 
             int opcion = scanner.nextInt();
-            scanner.nextLine(); // Consumir el salto de línea
+            scanner.nextLine();
 
             switch (opcion) {
                 case 1:
@@ -19,7 +20,7 @@ public class Main {
                 case 2:
                     break;
                 case 3:
-                    System.out.println("¡Hasta luego!");
+                    System.out.println("Fin");
                     return;
                 default:
                     System.out.println("Opción no válida. Por favor, seleccione una opción válida.");
